f205002abc92bbabb46bfb59aaae0f9371c3cf61
Yasmin Silva <alunoyasminsilva07@gmail.com>
2024-10-03 01:38:15 +0000

    benefícios finalizado

--- src/pages/Beneficios.jsx
+++ src/pages/Beneficios.jsx
@@ -2,71 +2,69 @@ import certo from '../assets/iconeCerto.svg'
 import imagensBeneficios from '../assets/imagensBeneficios.svg'
 
 export default function Beneficios() {
-  return (
-    <div className='
-    md:flex
-    '>
-        <div className='
-        '>
-            <h2 className='
-            2xl:w-3/5 2xl:mb-18
-            xl:text-[35px] xl:w-4/5 xl:mb-16
-            lg:text-[26px] lg:w-3/4 lg:mb-8
-            md:w-3/4 md:mb-8
-            font-karla font-bold text-[23px] mb-16'>Para nós, a qualidade do <br></br> seu sono é <span className='text-roxo-titulo'>prioridade</span></h2>
-            <p className='
-            2xl:w-3/5 2xl:mb-18
-            xl:text-[18px] xl:mb-16 xl:w-3/5
-            lg:mb-10 lg:w-3/4
-            md:w-3/4 md:mb-8
-            font-overpass text-[16px] mb-16 text-justify'>Dormir bem é ser saudável. Conheça alguns dos benefícios que você vai ter ao adquirir o Cloudy:</p>
+    return (
+
+        <div className='md:flex md:gap-10 lg:gap-6 xl:gap-16 2xl:gap-20'>
+
+            <div className='md:order-2 md:flex-1'>
+                <h2 className='
+            font-karla font-bold text-[23px] mb-16
+            md:w-[100%] md:text-[3vw] md:mb-[4vw]
+            lg:text-[2.55vw] lg:mb-[3.2vw]
+            xl:text-[2.73vw]
+            2xl:text-[2.05vw]
+          '>
+                    Para nós, a qualidade do <br></br> seu sono é <span className='text-roxo-titulo'>prioridade</span>
+                </h2>
+                <p className='
+            font-overpass text-[16px] mb-16 text-justify
+            md:w-[100%] md:text-[2vw] md:mb-[4vw]
+            lg:text-[1.6vw] lg:mb-[3.2vw]
+            xl:text-[1.4vw]
+            2xl:text-[1.04vw]
+          '>
+                    Dormir bem é ser saudável. Conheça alguns dos benefícios que você vai ter ao adquirir o Cloudy:
+                </p>
+
+                <div className='md:flex md:flex-wrap md:gap-6 md:items-center xl:gap-10'>
+                    <div className='md:flex-grow md:basis-[45%] flex flex-col justify-between'>
+                        <div className='flex gap-2 mb-2.5 md:mb-4 items-center xl:gap-4'>
+                            <img className='w-5 md:w-4 xl:w-5' src={certo} />
+                            <p className='font-overpass text-[16px] md:text-[1.8vw] lg:text-[1.6vw] xl:text-[1.4vw] 2xl:text-[1.04vw]'>Redução da insônia</p>
+                        </div>
+                        <div className='flex gap-2 mb-2.5 md:mb-4 items-center xl:gap-4'>
+                            <img className='w-5 md:w-4 xl:w-5' src={certo} />
+                            <p className='font-overpass text-[16px] md:text-[1.8vw] lg:text-[1.6vw] xl:text-[1.4vw] 2xl:text-[1.04vw]'>Criação de hábitos saudáveis de sono</p>
+                        </div>
+                        <div className='flex gap-2 mb-2.5 md:mb-4 items-center xl:gap-4'>
+                            <img className='w-5 md:w-4 xl:w-5' src={certo} />
+                            <p className='font-overpass text-[16px] md:text-[1.8vw] lg:text-[1.6vw] xl:text-[1.4vw] 2xl:text-[1.04vw]'>Melhora na qualidade do sono</p>
+                        </div>
+                    </div>
+
+                    <div className='md:flex-grow md:basis-[45%] flex flex-col justify-between'>
+                        <div className='flex gap-2 mb-2.5 md:mb-4 items-center xl:gap-4'>
+                            <img className='w-5 md:w-4 xl:w-5' src={certo} />
+                            <p className='font-overpass text-[16px] md:text-[1.8vw] lg:text-[1.6vw] xl:text-[1.4vw] 2xl:text-[1.04vw]'>Aumento da energia diurna</p>
+                        </div>
+                        <div className='flex gap-2 mb-2.5 md:mb-4 items-center xl:gap-4'>
+                            <img className='w-5 md:w-4 xl:w-5' src={certo} />
+                            <p className='font-overpass text-[16px] md:text-[1.8vw] lg:text-[1.6vw] xl:text-[1.4vw] 2xl:text-[1.04vw]'>Diminuição do estresse</p>
+                        </div>
+                        <div className='flex gap-2 mb-2.5 md:mb-4 items-center xl:gap-4'>
+                            <img className='w-5 md:w-4 xl:w-5' src={certo} />
+                            <p className='font-overpass text-[16px] md:text-[1.8vw] lg:text-[1.6vw] xl:text-[1.4vw] 2xl:text-[1.04vw]'>Conveniência e facilidade de uso</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
             <div className='
-            lg:flex lg:gap-6 2xl:gap-10
-            '>
-                <div className='2xl:w-1/4 xl:w-1/3 lg:w-2/5'>
-                    <div className='lg:gap-3 lg:mb-5 flex gap-2 mb-2.5'>
-                        <img className='w-5' src={certo} />
-                        <p className='xl:text-[18px] lg:leading-5 font-overpass text-[16px]'>Redução da insônia</p>
-                    </div>
-                    <div className='lg:gap-3 lg:mb-5 flex gap-2 mb-2.5'>
-                        <img className='w-5' src={certo} />
-                        <p className='xl:text-[18px] lg:leading-5 font-overpass text-[16px]'>Criação de hábitos saudáveis de sono</p>
-                    </div>
-                    <div className='lg:gap-3 lg:mb-5 flex gap-2 mb-2.5'>
-                        <img className='w-5' src={certo} />
-                        <p className='xl:text-[18px] lg:leading-5 font-overpass text-[16px]'>Melhora na qualidade do sono</p>
-                    </div>
-                </div>
-                
-                <div className='
-                2xl:w-1/4
-                xl:w-1/3
-                lg:w-2/5'>
-                    <div className='lg:gap-3 flex gap-2 mb-2.5'>
-                        <img className='w-5' src={certo} />
-                        <p className='xl:text-[18px] lg:leading-5 font-overpass text-[16px]'>Aumento da energia diurna</p>
-                    </div>
-                    <div className='lg:gap-3 flex gap-2 mb-2.5'>
-                        <img className='w-5' src={certo} />
-                        <p className='xl:text-[18px] lg:leading-5 font-overpass text-[16px]'>Diminuição do estresse</p>
-                    </div>
-                    <div className='lg:gap-3 flex gap-2 mb-2.5'>
-                        <img className='w-5' src={certo} />
-                        <p className='xl:text-[18px] lg:leading-5 font-overpass text-[16px]'>Conveniência e facilidade de uso</p>
-                    </div>
-                </div>
-            </div>    
-        </div>
-        <div>
-            <div className='
-            2xl:mt-[0px]
-            xl:mt-[28px]
-            lg:mt-[35px]
-            md:flex md:mt-[135px] md:mr-4'>
-                <img className='mt-16' src={imagensBeneficios} alt="" />
+            md:flex-1 md:flex md:justify-center md:items-align'>
+                <img className='mt-16 md:mt-12 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-[500px] xl:h-[380px] xl:mt-6' src={imagensBeneficios} alt="Benefícios" />
             </div>
         </div>
-    </div>
-  )
+    )
 }
+
